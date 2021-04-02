eedd3f66b70357899180ca80e9893bf37da6c55e
Steven de Oliveira <de.oliveira.steven@gmail.com>
2021-04-02 16:57:36 +0200

    Subdirectories for alcotest

--- src/ocaml-solidity-alcotest/main.ml
+++ src/ocaml-solidity-alcotest/main.ml
@@ -1,9 +1,9 @@
 exception Success
 exception Fail
 
-let test_dir = "test/raw_tests/"
-let fail_dir = test_dir ^ "fails"
-let success_dir = test_dir ^ "successes"
+let test_dir = "test/raw_tests"
+let fail_dir = test_dir ^ "/fails"
+let success_dir = test_dir ^ "/successes"
 
 let spf = Format.sprintf
 
@@ -31,18 +31,30 @@ let fail f = f, fun () ->
     Fail
     (check_file f)
 
+let get_files dir =
+  let rec loop acc subdir : string list =
+    let content = Sys.readdir subdir in
+    Array.fold_left
+      (
+        fun acc file ->
+          let full_path_file = spf "%s/%s" subdir file in
+          try loop acc full_path_file with
+          | Sys_error _s (* file is not a directory *) ->
+              full_path_file :: acc
+      )
+      acc
+      content
+  in loop [] dir
 
 let ok_files =
-  Array.to_list @@
-  Array.map
+  List.map
     success
-    (Array.map (spf "%s/%s" success_dir) (Sys.readdir success_dir))
+    (get_files success_dir)
 
 let ko_files =
-  Array.to_list @@
-  Array.map
+  List.map
     fail
-    (Array.map (spf "%s/%s" fail_dir) (Sys.readdir fail_dir))
+    (get_files fail_dir)
 
 let test_single_set =
   List.map
