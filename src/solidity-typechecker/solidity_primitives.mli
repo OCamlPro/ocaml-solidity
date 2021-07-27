c1eafd0777eb22039f8ce352cea2b92fccd86434
Fabrice Le Fessant <fabrice.le_fessant@origin-labs.com>
2021-07-28 00:30:02 +0200

    add variable assignments to annotations

--- src/solidity-typechecker/solidity_primitives.mli
+++ src/solidity-typechecker/solidity_primitives.mli
@@ -11,3 +11,37 @@
 (**************************************************************************)
 
 val init : unit -> unit
+
+module UTILS : sig
+  val register :
+    int ->
+    Solidity_common.primitive ->
+    (Solidity_common.pos ->
+     Solidity_checker_TYPES.options ->
+     Solidity_checker_TYPES.type_ option ->
+     Solidity_checker_TYPES.ident_desc option) ->
+    unit
+  val primitive_fun_named :
+    ?returns_lvalue:bool ->
+    (Solidity_checker_TYPES.type_ *
+     Solidity_common.IdentSet.elt option)
+      list ->
+    Solidity_checker_TYPES.type_ list ->
+    Solidity_ast.fun_mutability -> Solidity_checker_TYPES.ident_desc
+
+  val make_var :
+    Solidity_checker_TYPES.type_ -> Solidity_checker_TYPES.ident_desc
+  val make_fun :
+    ?returns_lvalue:bool ->
+    Solidity_checker_TYPES.type_ list ->
+    Solidity_checker_TYPES.type_ list ->
+    Solidity_ast.fun_mutability -> Solidity_checker_TYPES.ident_desc
+  val make_prim_args :
+    Solidity_common.pos ->
+    Solidity_checker_TYPES.options ->
+    Solidity_checker_TYPES.type_ list option
+  val preprocess_arg_0 : 'a -> 'b list option -> 'b list
+  val preprocess_arg_1 :
+    Solidity_common.pos -> 'a -> 'a list option -> 'a list
+
+end
